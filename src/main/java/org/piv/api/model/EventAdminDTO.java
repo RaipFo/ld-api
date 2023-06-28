25a11949e2ef665570f15166ee4be03bf2eda3a7
RaipFo <65996012+RaipFo@users.noreply.github.com>
2023-06-28 13:36:27 +0300

    Added DTO

---
+++ src/main/java/org/piv/api/model/EventAdminDTO.java
@@ -0,0 +1,20 @@
+package org.piv.api.model;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class EventAdminDTO {
+    private Long id;
+    private String org_name;
+}
---
+++ src/main/java/org/piv/api/model/EventDTO.java
@@ -0,0 +1,22 @@
+package org.piv.api.model;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class EventDTO {
+    private Long id;
+    private Long cost;
+    private String info;
+    private EventAdminDTO eventAdmin;
+}
